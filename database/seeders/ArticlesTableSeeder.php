d8fe3fa7dbdb7c119609f2a02d915e3789491775
ogasawarayui <ogasawara.yui@outlook.com>
2024-08-05 16:50:46 +0900

    app

--- database/seeders/ArticlesTableSeeder.php
+++ database/seeders/ArticlesTableSeeder.php
@@ -4,14 +4,28 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ArticlesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('articles')->insert([
+            [
+                'title' => 'タイトル1',
+                'body' => '内容1'
+            ],
+            [
+                'title' => 'タイトル2',
+                'body' => '内容2'
+            ],
+            [
+                'title' => 'タイトル3',
+                'body' => '内容3'
+            ],
+        ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
         // User::factory(10)->create();
-
+        $this->call(ArticlesTableSeeder::class);
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
